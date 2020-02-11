8b9f75390271a03700ac909f3bdb29c8d2e41b3f
ask098 <cmlocastro20@gmail.com>
2020-02-11 14:01:39 -0500

    initial state and food for the snake game. using model-actions-view model

---
+++ snake.rb
@@ -0,0 +1,16 @@
+# set the windows size, and a red square
+# this is not necesary for the game, its just to practice
+
+require 'ruby2d'
+
+# Set the window size
+set width: 300, height: 200
+
+# Create a new shape
+s = Square.new
+
+# Give it some color
+s.color = 'red'
+
+# Show the window
+show
---
+++ src/model/state.rb
@@ -0,0 +1,38 @@
+# Its the representation state of the game
+# using as a model to be called.
+module Model
+    # set the arguments needed to Coordenades
+    class Coord < Struct.new(:row, :col)
+    end
+
+    # define the food in heritage of coords method
+    class Food < Coord
+    end
+
+    # used to render the positions for the snake
+    class Snake < Struct.new(:positions)
+    end
+
+    # define the rows and columns for the grid
+    class Grid < Struct.new(:row, :col)
+    end
+
+    # define the state for the snake
+    class State < Struct.new(:snake, :food, :grid)
+    end
+
+    # set the initial state for the snake
+    def self.initial_state
+        Model::State.new(
+            Model::Snake.new([
+                # call and set the coord fo the snake
+                Model::Coord.new(1, 1),
+                Model::Coord.new(0, 1),
+                ]),
+            # call and set the coord for the food
+            Model::Food.new(4, 4),
+            Model::Grid.new(9, 12)
+        )
+    end
+
+end
